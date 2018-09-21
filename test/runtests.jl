9aafea5f112628158cb36b5df8387a162e3b39d9
Erik Schnetter <schnetter@gmail.com>
2018-09-21 14:41:37 -0400

    Test some ring properties

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,18 @@ using Example2018
 # 4. addition commutes
 # 5. multiplication is ...
 
-z = Polynomial([0])
-p = Polynomial([1, 2, 3])
+@testset "Polynomials form a ring" begin
 
-@test p + z == p
+    z = Polynomial([0])
+
+    for n in 1:100
+
+        deg = rand(0:100)
+        p = Polynomial(rand(-1000000:1000000, deg+1))
+    
+        @test p + z == p
+        @test z + p == p
+        @test p + (-p) == z
+    end
+
+end
